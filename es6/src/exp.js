fb4ba697978c470a39e6fb34e3a144674dde3e03
bryan sun <hitsjt@gmail.com>
2019-08-04 23:25:35 +0800

    js base

---
+++ es6/src/exp.js
@@ -0,0 +1,6 @@
+// export let a=10;
+// export let foo=()=>{
+//     return "hello"
+// }
+let a=10;
+export default a;
---
+++ es6/src/imp.js
@@ -0,0 +1,9 @@
+// import {a,foo} from "./exp.js"
+// console.log(a);
+// console.log(foo());
+//可以多个
+
+
+import aaa from "./exp.js"
+console.log(aaa);
+
