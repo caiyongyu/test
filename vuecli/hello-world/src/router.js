6fe8f4b00c058e1bd050d7fb75448a15c42b39f3
caiyongyu <1466401937@qq.com>
2019-08-18 23:28:59 +0800

    vuecli

--- vuecli/hello-world/src/router.js
+++ vuecli/hello-world/src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
@@ -46,6 +46,10 @@ export default new Router({
     {
       path    : "/mine",
       redirect: "/"
+    },
+    {
+      path:"*",
+      component: ()=>import('./views/Error404.vue')
     }
   ]
 })
