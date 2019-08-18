37abf10f86be15ba68c57447fb2621119ba135d2
caiyongyu <1466401937@qq.com>
2019-08-18 22:49:02 +0800

    vuecli

--- vuecli/hello-world/src/views/Test2.vue
+++ vuecli/hello-world/src/views/Test2.vue
@@ -3,5 +3,46 @@
         <h1>this is test2 page1</h1>
         <h1>年龄：{{$route.params.username}}</h1>
         <h1>姓名：{{$route.params.userage}}</h1>
+        <div>
+            <button @click="isShow=!isShow">click</button>
+        </div>
+        <transition name="fade">
+            <div v-show="isShow">hello world</div>
+        </transition>
+        
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+<style scoped>
+    .fade-enter {
+        opacity: 0;
+    }
+    
+    .fade-enter-active {
+        transition: all 1s ease;
+    }
+
+    .fade-enter-to {
+        opacity: 1;
+    }
+
+    .fade-leave {
+        opacity: 1;
+    }
+
+    .fade-leave-active {
+        transition: all 1s ease;
+    }
+
+    .fade-leave-to {
+        opacity: 0;
+    }
+</style>
