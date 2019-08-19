c2ed4483720fa9f6d5d403713fc7bd3bde28e6b6
caiyongyu <1466401937@qq.com>
2019-08-19 18:06:08 +0800

    vuex

--- vuecli/hello-world/src/views/Test3.vue
+++ vuecli/hello-world/src/views/Test3.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-        <button @click="$store.commit('add')">加一</button>
-        <button @click="$store.commit('reduce')">减一</button>
-        <h1>{{$store.state.count}}</h1>
+        <button @click="addHandle">加一</button>
+        <button @click="reduceHandle">减一</button>
+        <h1>{{count}}</h1>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        addHandle(){
+            this.$store.commit('add')
+        },
+        reduceHandle(){
+            this.$store.commit('reduce')
+        }
+    },
+    computed: {
+        count(){
+            return this.$store.state.count+10
+        }
+    }
 }
 </script>
 <style scoped>
