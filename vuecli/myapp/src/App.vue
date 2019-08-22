4fe75f1e8dc11ead419317f14155fec9c63132e9
caiyongyu <1466401937@qq.com>
2019-08-22 21:23:09 +0800

    vueapp

---
+++ vuecli/myapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Navgation></Navgation>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navgation from "@/components/Navagation.vue"
+export default {
+  components:{
+    Navgation
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuecli/myapp/src/components/Navagation.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav-container">
+        <header>
+            <p>首页</p>
+            <span>电影</span>
+        </header>
+        <nav>
+            <ul>
+                <li>电影</li>
+                <li>音乐</li>
+                <li>书籍</li>
+                <li>聊天</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .nav-container header,.nav-container nav {
+        height: 1rem;
+        background-color: gray;
+    }
+</style>
