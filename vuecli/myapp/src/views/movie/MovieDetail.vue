6f3ffca1c6347fdf9b895eadf9c6f93aed077f97
caiyongyu <1466401937@qq.com>
2019-08-24 12:33:17 +0800

    vueapp

--- vuecli/myapp/src/views/movie/MovieDetail.vue
+++ vuecli/myapp/src/views/movie/MovieDetail.vue
@@ -6,7 +6,19 @@
 
 <script>
 export default {
-    
+    created(){
+        console.log(this.$route.params.id);
+        this.getData(this.$route.params.id);
+    },
+    methods:{
+        getData(id){
+            axios.get("https://bird.ioliu.cn/v2?url=https://api.douban.com/v2/movie/subject/"+id)
+            .then((res)=>{
+                console.log(res.data);
+                
+            })
+        }
+    }
 }
 </script>
 
