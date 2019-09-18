f6f3c82a66e07afe0fc8635a607b064923c42450
caiyongyu <1466401937@qq.com>
2019-09-18 20:30:07 +0800

    vue-cli

--- vue-cli/sell/src/App.vue
+++ vue-cli/sell/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div class="header">
-      I am header!
-    </div>
+    <v-header></v-header>
     <div class="tab">
-      I am tab!
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
     </div>
     <div class="content">
       I am content!
@@ -13,9 +13,23 @@
 </template>
 
 <script>
+    import header from './components/header/header.vue';
 
+    export default {
+      components: {
+        'v-header': header
+      }
+    };
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height :40px
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
---
+++ vue-cli/sell/src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        I am header
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {};
+    // 空行
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
