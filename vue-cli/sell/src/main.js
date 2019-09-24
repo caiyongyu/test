f40336f81d70ffed13108c213a4f7e98e05ab960
caiyongyu <1466401937@qq.com>
2019-09-24 23:57:24 +0800

    shengtou

--- vue-cli/sell/src/main.js
+++ vue-cli/sell/src/main.js
@@ -3,13 +3,37 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods';
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-});
+
+
+Vue.use(VueRouter);
+
+// let app=Vue.extend(App);
+
+// let router=new VueRouter();
+
+// router.map({
+//   '/goods': {
+//     components: goods
+//   }
+// });
+
+// router.start(app, '#app');
+
+const routes = [
+  { path: '/goods', component: goods }
+  // { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
+
