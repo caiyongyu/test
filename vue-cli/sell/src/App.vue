7532cab3add0a8318c1f93b5e9c0c5bb3384f639
caiyongyu <1466401937@qq.com>
2019-10-12 10:18:15 +0800

    vue-cli

--- vue-cli/sell/src/App.vue
+++ vue-cli/sell/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+        </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      I am content!
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -23,7 +27,6 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
     .tab
       display: flex
       width: 100%
--- vue-cli/sell/src/main.js
+++ vue-cli/sell/src/main.js
@@ -2,14 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+// import router from './router';
 import VueRouter from 'vue-router';
 import goods from './components/goods/goods';
 
-Vue.config.productionTip = false;
-
-
-
+// Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 // let app=Vue.extend(App);
@@ -24,16 +21,22 @@ Vue.use(VueRouter);
 
 // router.start(app, '#app');
 
-const routes = [
-  { path: '/goods', component: goods }
-  // { path: '/bar', component: Bar }
-]
+const routes = [{
+   path: '/',
+   component: goods
+}, {
+    path: '/goods',
+    component: goods
+}];
 
 const router = new VueRouter({
+  linkActiveClass: 'active',
   routes // (缩写) 相当于 routes: routes
-})
+});
 
-const app = new Vue({
-  router
-}).$mount('#app')
-
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
