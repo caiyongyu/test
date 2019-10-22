1b9f45c3abee67c1c2331b24da2651f626d11df7
caiyongyu <1466401937@qq.com>
2019-10-22 12:21:09 +0800

    vue-cli

--- vue-cli/sell/src/components/cartcontrol/cartcontrol.vue
+++ vue-cli/sell/src/components/cartcontrol/cartcontrol.vue
@@ -30,6 +30,7 @@ export default {
             } else {
                 this.food.count++;
             }
+            this.$emit('add', event.target);
         },
         decreaseCart (event) {
             if (!event._constructed) {
--- vue-cli/sell/src/components/goods/goods.vue
+++ vue-cli/sell/src/components/goods/goods.vue
@@ -28,7 +28,7 @@
                                     <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                                 </div>
                                 <div class="cartcontrol-wrapper">
-                                    <cartcontrol :food="food"></cartcontrol>
+                                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
                                 </div>
                             </div>
                         </li>
@@ -36,7 +36,7 @@
                 </li>
             </ul>
         </div>
-        <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+        <shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
     </div>
 </template>
 
@@ -83,6 +83,11 @@ export default{
             return foods;
         }
     },
+    // events: {
+    //     'cart.add'(target) {
+    //         this._drop(target);
+    //     }
+    // },
     created () {
         this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 
@@ -106,7 +111,16 @@ export default{
             let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
             let el = foodList[index];
             this.foodsScroll.scrollToElement(el, 300);
-            console.log(index);
+            // console.log(index);
+        },
+        addFood(target) {
+            this._drop(target);
+        },
+        _drop (target) {
+            // 体验优化，异步执行下落动画
+            this.$nextTick(() => {
+                this.$refs.shopcart.drop(target);
+            });
         },
         _initScroll () {
             this.menuScroll = new BScroll(this.$refs.menuWrapper, {
