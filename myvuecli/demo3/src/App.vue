fbb14643af91c85260c2ba79b79e15469ee4c666
caiyongyu <1466401937@qq.com>
2019-12-18 00:29:29 +0800

    myvuecli

---
+++ myvuecli/demo3/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <h1>这是h1</h1>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="stylus">
+@import '~@/assets/style.styl'
+.main
+  background: $bg
+  
+</style>
+
+
