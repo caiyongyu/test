67d52bfdcfe18c870d4297d3909d0a2528826eb2
caiyongyu <1466401937@qq.com>
2020-03-09 11:08:13 +0800

    offer

---
+++ 222.面试知识点补漏/1.webpack/src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+console.log(111);
+let a=10;
+console.log(a)
---
+++ 222.面试知识点补漏/1.webpack/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+// const ExtracttextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode:"development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer:{
+    contentBase:"./dist",
+    inline:true
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader','css-loader'] },
+        {
+            test:/\.(js|jsx)$/,       //匹配文件夹中后缀名是 .js的文件（注意这里不能加 引号）
+            exclude:/node_modules/, //排除 node_modules 中的js文件（注意这里不能加引号）
+            loader:"babel-loader"            //对匹配的 js文件用 babel来编译
+        },
+
+//blog.csdn.net/hjh15827475896/article/details/86174626
+      // { test: /\.css$/, use: ExtracttextPlugin.extract({
+      //   fallback:"style-loader",
+      //   use:[{
+      //     loader:'css-loader',
+      //     options:{
+      //       minimize:true
+      //     }
+      //   }],
+      //   publicPath:"../"
+      // }) },
+      { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'file-loader?name=img/[name].[ext]' },
+      { test: /\.html$/, use: 'html-withimg-loader' }
+    ]
+  },
+  plugins:[
+    // new ExtracttextPlugin('css/[name].css'),
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',
+      minify:{
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+    
+    //是否移除注释 默认false
+    removeComments: true,
+    removeEmptyAttributes: true
+      }
+    })
+    
+  ]
+};
