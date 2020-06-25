d0f0a58de65ccbc3c52bf59d5600a77df66136c7
caiyongyu <1466401937@qq.com>
2020-06-25 17:30:27 +0800

    6.25

---
+++ wuliu system/wuliu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      hello wuliu
+    </div>
+  );
+}
+
+export default App;
